0f16644ba84702b0d36e1abd2ebef2fe12c774cc
yimei <yimei.winata@binus.ac.id>
2025-06-08 00:32:18 +0700

    tambah-register

--- app/Http/Controllers/RegisterAddressController.php
+++ app/Http/Controllers/RegisterAddressController.php
@@ -3,28 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Provinces;
+use App\Models\Cities;
 
 class RegisterAddressController extends Controller
 {
     public function index()
     {
-        return view('auth.register-address');
+        $provinces = Provinces::all();
+        return view('auth.register-address', compact('provinces'));
     }
 
     public function registerAddress(Request $request)
     {
         $request->validate([
-            'address' => 'required|string|max:255',
+            'gender' => 'required|string|max:10',
+            'dob' => 'required|date',
+            'province' => 'required|string|max:100',
             'city' => 'required|string|max:100',
-            'zipcode' => 'required|string|max:20',
         ]);
 
         $user = auth()->user();
-        $user->address = $request->address;
+        $user->gender = $request->gender;
+        $user->dob = $request->dob;
+        $user->province_id = $request->province; 
         $user->city = $request->city;
-        $user->zipcode = $request->zipcode;
         $user->save();
 
         return redirect()->route('dashboard');
+    }
+
+    public function getCities($provinceId)
+    {
+        $cities = Cities::where('province_id', $provinceId)->get(['id', 'name']);
+        return response()->json($cities);
     }
 }
